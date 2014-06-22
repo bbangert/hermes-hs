9f6390702cd4673c7c4fc4e572926c74dc014e06
Ben Bangert <ben@groovie.org>
2014-06-21 22:55:26 -0700

    we're civilized adults with screens wider than 80 characters.

--- cn/Main.hs
+++ cn/Main.hs
@@ -54,8 +54,7 @@ nackDeviceMessage :: DeviceMessage -> ServerState -> IO ()
 nackDeviceMessage (deviceId, (Outgoing s mid _)) state = do
   cdata <- atomically $ getClient deviceId state
   case cdata of
-    Just (_, _, conn) ->
-      WS.sendTextData conn $ T.concat ["OUT:", s, ":", mid, ":NACK"]
+    Just (_, _, conn) -> WS.sendTextData conn $ T.concat ["OUT:", s, ":", mid, ":NACK"]
     _ -> return ()
 nackDeviceMessage _ _ = return ()
 
@@ -93,9 +92,8 @@ removeClient deviceId state = modifyTVar' (clientMap state) $ M.delete deviceId
 getClient :: DeviceID -> ServerState -> STM (Maybe ClientData)
 getClient deviceId state = readClientMap state >>= return . (M.lookup deviceId)
 
--- Read a message and enforce a timeout. The response will be in a Maybe
--- indicating whether it timed out or not. If it didn't time out, then the
--- Either will be an indication if the packet it found parsed or not.
+-- Read a message and enforce a timeout. The response will be in a Maybe indicating whether it timed out or
+-- not. If it didn't time out, then the Either will be an indication if the packet it found parsed or not.
 readData :: WS.Connection -> Int -> IO (Maybe (Either String ClientPacket))
 readData conn after = timeout after $ parseMessage <$> WS.receiveData conn
 
@@ -109,8 +107,7 @@ main = do
 parseMessage :: Text -> Either String W.ClientPacket
 parseMessage = A.parseOnly (W.clientPacketParser <* A.endOfInput) . T.strip
 
--- We start our interaction with a new websocket here, to do the basic HELO
--- exchange
+-- We start our interaction with a new websocket here, to do the basic HELO exchange
 application :: ServerState -> WS.ServerApp
 application state pending = do
   conn <- WS.acceptRequest pending
@@ -119,13 +116,13 @@ application state pending = do
   putStrLn (show msg)
   case msg of
     Right (Helo ver ping) ->
-      if ver == 1 then
-        WS.sendTextData conn ("HELO:v1" :: Text) >> checkAuth state conn (ping*1000000)
-      else return ()
+      if ver == 1
+        then WS.sendTextData conn ("HELO:v1" :: Text) >> checkAuth state conn (ping*1000000)
+        else return ()
     _ -> return ()
 
--- Second state of a new websocket exchange, HELO worked, now its on to doing
--- the AUTH before we transition to message sending/receiving mode
+-- Second state of a new websocket exchange, HELO worked, now its on to doing the AUTH before we transition
+-- to message sending/receiving mode
 checkAuth :: ServerState -> WS.Connection -> PingInterval -> IO ()
 checkAuth state conn ping = do
   myId <- myThreadId
@@ -139,18 +136,15 @@ checkAuth state conn ping = do
       deviceID <- W.newDeviceID
       atomically $ addClient deviceID cdata state
       safeCleanup deviceID $ do
-        WS.sendTextData conn $ T.concat [
-          "AUTH:NEW:", deviceID, ":",
-           W.signDeviceID deviceID "secret"]
+        WS.sendTextData conn $ T.concat ["AUTH:NEW:", deviceID, ":", W.signDeviceID deviceID "secret"]
         messagingApplication state deviceID cdata
 
     process (Right auth@(ExistingAuth deviceID _)) cdata
       | W.verifyAuth "secret" auth = do
 
-        -- Atomically determine if this DeviceID already exists in the server
-        -- states client map. If it does, return an IO op that will kill the dupe
-        -- client. Either way we update the client map so that the DeviceID now points
-        -- to our current client data.
+        -- Atomically determine if this DeviceID already exists in the server states client map. If it does,
+        -- return an IO op that will kill the dupe client. Either way we update the client map so that the
+        -- DeviceID now points to our current client data.
         -- We return an IO op to run, since the STM can't run IO ops, ergo the 'join'.
         join . atomically $ do
           client <- getClient deviceID state
@@ -165,10 +159,9 @@ checkAuth state conn ping = do
 
     process _ _ = WS.sendTextData conn ("AUTH:INVALID" :: Text)
 
-    -- Define a safe clean-up that ensures if the rest of this clients interaction goes
-    -- bad, we will ALWAYS remove this client from the client mapping. Note that we
-    -- have to check to ensure we don't delete this deviceId if the threadId doesn't match
-    -- our own threadId (ie, maybe we've been killed).
+    -- Define a safe clean-up that ensures if the rest of this clients interaction goes bad, we will ALWAYS
+    -- remove this client from the client mapping. Note that we have to check to ensure we don't delete this
+    -- deviceId if the threadId doesn't match our own threadId (ie, maybe we've been killed).
     safeCleanup deviceId = flip finally $ do
       myId <- myThreadId
       atomically $ do
@@ -178,25 +171,23 @@ checkAuth state conn ping = do
           Just (_, cid, _) -> if myId == cid then removeClient deviceId state
             else return ()
 
--- Final transition to message send/receive mode. This runs until the client
--- does something bad which will cause the connection to drop.
+-- Final transition to message send/receive mode. This runs until the client does something bad which will
+-- cause the connection to drop.
 messagingApplication :: ServerState -> DeviceID -> ClientData -> IO ()
 messagingApplication state uuid (ping, _, conn) = forever $ do
   pmsg <- readData conn ping
 
-  -- We have a nested case here of an Either inside a Maybe. The Maybe indicates
-  -- whether or not we timed out attempting to read data. The Either indicates if
-  -- the message parsed or not. We only accept Outgoing/Ping messages here, all else
-  -- results in dropping the connection.
+  -- We have a nested case here of an Either inside a Maybe. The Maybe indicates whether or not we timed out
+  -- attempting to read data. The Either indicates if the message parsed or not. We only accept Outgoing/Ping
+  -- messages here, all else results in dropping the connection.
   case pmsg of
     Just (Right packet@(Outgoing s mid _)) -> do
       print packet
 
-      -- Run an STM atomic operation that reads the list of available relays and
-      -- attempts to put the message on a bounded queue (tbqueue) of the first relay
-      -- available. If no relay is available, we NACK the message. If the relay's
-      -- queue is full, this blocks until either the relay list is updated
-      -- (relay is removed/added) or the queue has space.
+      -- Run an STM atomic operation that reads the list of available relays and attempts to put the message
+      -- on a bounded queue (tbqueue) of the first relay available. If no relay is available, we NACK the
+      -- message. If the relay's queue is full, this blocks until either the relay list is updated (relay is
+      -- removed/added) or the queue has space.
       join . atomically $ do
         rs <- readTVar $ relays state
         if null rs then do
