98b7b616b5487c1e3e0219b67c00154f6ea8478f
Ben Bangert <ben@groovie.org>
2014-07-16 09:36:35 -0700

    Switch to using socket directly.

--- hermes-cn/Hermes/ConnectionNode/RelayClient.hs
+++ hermes-cn/Hermes/ConnectionNode/RelayClient.hs
@@ -13,19 +13,32 @@ import           Control.Concurrent.STM.TVar           (TVar, newTVarIO,
 import           Control.Exception                     (bracket, bracket_,
                                                         catch)
 import           Control.Monad                         (unless)
+import           Control.Monad.IO.Class                (liftIO)
 import           Control.Monad.STM                     (atomically)
 import           Data.Attoparsec.ByteString            (parseOnly)
 import           Data.Binary                           (encode)
 import           Data.ByteString                       (ByteString)
 import qualified Data.ByteString                       as B
 import qualified Data.ByteString.Lazy                  as BL
+import           Data.Conduit                          (Sink, awaitForever,
+                                                        ($$), ($=))
+import           Data.Conduit.Attoparsec               (ParseError,
+                                                        PositionRange,
+                                                        conduitParserEither)
+import qualified Data.Conduit.Network                  as CN
 import           Data.Map.Strict                       (Map)
 import qualified Data.Map.Strict                       as M
-import           Network                               (HostName,
-                                                        PortID (PortNumber),
-                                                        connectTo)
+import           Network.Socket                        (AddrInfo (..),
+                                                        Family (..), Socket,
+                                                        SocketOption (..),
+                                                        SocketType (..),
+                                                        addrFamily, connect,
+                                                        getAddrInfo, sClose,
+                                                        setSocketOption, socket,
+                                                        withSocketsDo)
+import           Network.Socket.ByteString             (recv, sendAll)
 import qualified Network.Wai.Handler.Warp.Timeout      as WT
-import           System.IO                             (hSetBinaryMode)
+import           System.IO                             (hClose, hSetBinaryMode)
 import           System.Posix.Signals                  (Handler (Ignore),
                                                         installHandler, sigPIPE)
 
@@ -41,32 +54,43 @@ import           Hermes.Protocol.Binary                (RelayClientPacket (..),
 type PendingMap = Map ByteString DeviceMessage
 
 -- | Connect the relay client to a relay server.
-connectRelayClient :: HostName      -- ^ Relay hostname
+connectRelayClient :: String        -- ^ Relay hostname
                    -> Int           -- ^ Relay port number
                    -> ServerState   -- ^ Server state
                    -> IO ()
-connectRelayClient hostname port state = do
+connectRelayClient hostname port state = withSocketsDo $ do
     -- Writing to a dead socket gets a sigPIPE on linux, we'd prefer not to
     -- segfault when this happens
     _ <- installHandler sigPIPE Ignore Nothing
 
-    h <- connectTo hostname $ PortNumber (fromIntegral port)
-    hSetBinaryMode h True
-    BL.hPut h (encode $ ICHelo 1)
-    reply <- parseOnly relayServerParser <$> B.hGet h 6
-    case reply of
-        -- The right version match
-        Right (ISHelo 1 mb) -> do
-            myId <- myThreadId
-            ic <- T.newTBChanIO 50
-            pending <- newTVarIO M.empty
-            let inbr = InboundRelay myId ic
-            bracket_ (atomically $ addRelay inbr state)
-                     (cleanUpRelay inbr pending state)
-                     (processMessages inbr pending mb state)
+    bracket (socket AF_INET Stream 0)
+            (\sock -> sClose sock)
+            (\sock -> do
+                setSocketOption sock ReuseAddr 1
+                setSocketOption sock NoDelay 1
+                (ainfo:_) <- getAddrInfo Nothing (Just hostname) (Just $ show port)
+                let addr = addrAddress ainfo
+                connect sock (addrAddress ainfo)
+                go sock
+                )
 
-        -- Anything else, not going to work
-        _ -> return ()
+  where
+    go sock = do
+        sendAll sock (encode $ ICHelo 1)
+        reply <- parseOnly relayServerParser <$> recv sock 6
+        case reply of
+            -- The right version match
+            Right (ISHelo 1 mb) -> do
+                myId    <- myThreadId
+                ic      <- T.newTBChanIO 50
+                pending <- newTVarIO M.empty
+                let inbr = InboundRelay myId ic
+                bracket_ (atomically $ addRelay inbr state)
+                         (cleanUpRelay inbr pending state)
+                         (processMessages inbr pending mb state sock)
+
+            -- Anything else, not going to work
+            _ -> return ()
 
 -- | Ignore killthread
 ignoreThreadKill :: WT.TimeoutThread -> IO ()
@@ -86,7 +110,7 @@ drainChannel = go
 -- then attempt to move the messages to the other relays, otherwise NACK all
 -- the incoming messages. Pending messages are all dropped with no NACK's.
 cleanUpRelay :: InboundRelay -> TVar PendingMap -> ServerState -> IO ()
-cleanUpRelay inbr@(InboundRelay _ messageChan) pending state = do
+cleanUpRelay inbr@(InboundRelay _ messageChan) _ state = do
     atomically $ removeRelay inbr state
     bracket (WT.initialize 500000) WT.stopManager $ \tm -> do
 
@@ -117,6 +141,18 @@ processMessages :: InboundRelay     -- ^ This inbound relay
                 -> TVar PendingMap  -- ^ Pending messages to ack
                 -> Int              -- ^ Max pending messages
                 -> ServerState      -- ^ Global serverstate
+                -> Socket           -- ^ Input socket
                 -> IO ()
-processMessages (InboundRelay tid messageChan) pending maxBatch state = do
+processMessages (InboundRelay tid messageChan) pending maxBatch state sock = do
+
     return ()
+
+receiveLoop sock handler = do
+    result <- CN.sourceSocket sock $= conduitParserEither relayServerParser $$ sink
+    return result
+ where
+    sink :: Sink (Either ParseError (PositionRange, RelayServerPacket)) IO ()
+    sink = awaitForever $ \tc -> do
+        case tc of
+            Left err -> liftIO $ fail $ show err
+            Right (_, tc) -> liftIO $ handler tc
