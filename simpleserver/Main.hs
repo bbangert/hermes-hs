6291d087cc57db7db9e89926f0e82896db3125f9
Ben Bangert <ben@groovie.org>
2014-07-16 08:32:19 -0700

    Simplify the simple server.

--- simpleserver/Main.hs
+++ simpleserver/Main.hs
@@ -1,32 +1,31 @@
-{-# LANGUAGE TemplateHaskell    #-}
+{-# LANGUAGE QuasiQuotes     #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE TypeFamilies    #-}
 
 module Main
   (
     main
   ) where
 
-import           Control.Monad                    (forever)
-import           Data.ByteString                  (ByteString)
-import           Data.FileEmbed                   (embedDir)
-import qualified Network.Wai
-import qualified Network.Wai.Application.Static   as Static
-import qualified Network.Wai.Handler.Warp         as Warp
-import qualified Network.Wai.Handler.WebSockets   as WaiWS
-import qualified Network.WebSockets               as WS
+import           Conduit
+import           Control.Exception (finally)
+import           Control.Monad     (forever)
+import           Yesod.Core
+import           Yesod.WebSockets  (sinkWSBinary, sourceWS, webSockets)
+
+
+data App = App
+
+instance Yesod App
+
+mkYesod "App" [parseRoutes|
+/ HomeR GET
+|]
+
+getHomeR :: Handler Html
+getHomeR = do
+    webSockets $ forever $ sourceWS $$ sinkWSBinary
+    return $ return ()
 
 main :: IO ()
-main = do
-  putStrLn "All started, launching socket server..."
-  Warp.runSettings Warp.defaultSettings
-    { Warp.settingsPort = 8080
-    } $ WaiWS.websocketsOr WS.defaultConnectionOptions application staticApp
-
-staticApp :: Network.Wai.Application
-staticApp = Static.staticApp $ Static.embeddedSettings $(embedDir "static")
-
-application :: WS.ServerApp
-application pending = do
-  conn <- WS.acceptRequest pending
-  forever $ do
-      m <- WS.receiveData conn
-      WS.sendTextData conn (m :: ByteString)
+main = warp 8080 App
